2bd9d224a80b84530a57634d87c3df18b2579141
Tzu-ping Chung <uranusjr@gmail.com>
2014-08-31 01:33:36 +0800

    Add Heroku files

---
+++ __/settings/prod.py
@@ -0,0 +1,12 @@
+from .base import *     # noqa
+import dj_database_url
+
+SECRET_KEY = get_env_var('DJANGO_SECRET_KEY')
+
+DATABASES['default'] = dj_database_url.config()
+
+ALLOWED_HOSTS = ['djangogirlstw.herokuapp.com']
+
+STATIC_ROOT = 'static'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
