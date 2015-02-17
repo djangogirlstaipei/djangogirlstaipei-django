f743fec77e7090e3e0e7749ec8615fbf5523dbda
Tzu-ping Chung <uranusjr@gmail.com>
2015-02-17 19:40:52 +0800

    Fix URL pattern in new versions of Django

--- __/urls.py
+++ __/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     '',
     url(r'^ajax/', include('ajax.urls', namespace='ajax')),
-    url(r'^', include('pages.urls', namespace='pages')),
+    url(r'', include('pages.urls', namespace='pages')),
 )
