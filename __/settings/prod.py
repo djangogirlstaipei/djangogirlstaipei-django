9c0c94588271c3f213d3dc3b1f6504875ad1a17e
Tzu-ping Chung <uranusjr@gmail.com>
2018-04-27 13:54:41 +0800

    Switch to cookie-based sessions

--- __/settings/prod.py
+++ __/settings/prod.py
@@ -1,11 +1,14 @@
-from .base import *     # noqa
 import dj_database_url
+
+from .base import get_env_var, DATABASES
 
 SECRET_KEY = get_env_var('DJANGO_SECRET_KEY')
 
 DATABASES['default'] = dj_database_url.config()
 
 ALLOWED_HOSTS = ['djangogirlstaipei.herokuapp.com']
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 STATIC_ROOT = 'static'
 
